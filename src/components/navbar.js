791d3ecb1842fa08f2e514c7cb04a4282faf19df
bwaobikeze <bwaobikeze@hotmail.com>
2021-08-11 16:19:57 -0500

    added button coponents

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,6 +5,7 @@ function Navbar() {
     const [click,setClick]=useState(false);
 
     const handleClick= () =>setClick (!click);
+    const closeMobileMenu=()=> setClick(false);
     return (
         <>
         <nav className="navbar">
@@ -12,9 +13,37 @@ function Navbar() {
                 <Link to='/' className="navbar-logo">
                     PORTFOLIO <i className='far fa-address-book' />
                 </Link>
-                <div className='menu-icon'onClick= {handleClick}>
+                <div className='menu-icon' onClick= {handleClick}>
                     <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
                 </div>
+                <ul className={click ? 'nav-menu active' :  'nav-menu' }>
+                <li className='nav-item'>
+                    <Link to ='/' className='nav-links' onClick={closeMobileMenu}>
+                        Home
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to ='/AboutMe' className='nav-links' onClick={closeMobileMenu}>
+                        AboutMe
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to ='/Projects' className='nav-links' onClick={closeMobileMenu}>
+                        Projects
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to ='/Skills' className='nav-links' onClick={closeMobileMenu}>
+                        Skills
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to ='/ ContactMe' className='nav-links' onClick={closeMobileMenu}>
+                        ContactMe
+                    </Link>
+                </li>
+                </ul>
+                
             </div>
         </nav>
 
